3fa751f5578b6347b609f2b381060498b9a634fe
neopayvn <106588338+neopayvn@users.noreply.github.com>
2023-07-24 17:40:23 +0700

    Update createUrlRequest.js
    update test data

--- example/createUrlRequest.js
+++ example/createUrlRequest.js
@@ -1,10 +1,10 @@
 const crypto = require('crypto');
 
-const API_URL = 'https://uat-api.neopay.vn/pg/api/v1/'
+const API_URL = 'https://uat-api.neopay.vn/pg/api/v1/paygate/neopay'
 
 const hashSHA256 = (text) => {
-    const md5sum = crypto.createHash('sha256');
-    const res = md5sum.update(text).digest('hex');
+    const hashed = crypto.createHash('sha256');
+    const res = hashed.update(text).digest('hex');
     return res;
 }
 
@@ -32,27 +32,26 @@ const convertObjectToQuery = (obj) => {
 const generatorUrlRequest = (obj, secureHash) => {
     let query = convertObjectToQuery(obj);
     const neo_SecureHash = generatorSecureHash(obj, secureHash)
-    const url = `${API_URL}paygate/neopay?${query}&neo_SecureHash=${neo_SecureHash}`;
+    const url = `${API_URL}?${query}&neo_SecureHash=${neo_SecureHash}`;
     return url;
 }
 
 const runTest = () => {
     const obj = {
-        "neo_MerchantCode": "HUYEN1",
-        "neo_PaymentMethod": "WALLET, ATM, CC, QR",
+        "neo_MerchantCode": "SDTEST",
+        "neo_PaymentMethod": "WALLET,ATM,CC,QR",
         "neo_Currency": "VND",
         "neo_Locale": "vi",
         "neo_Version": "1",
         "neo_Command": "PAY",
         "neo_Amount": 10000,
-        "neo_MerchantTxnID": "T21895330",
-        "neo_OrderID": `DH${`${Date.now()}`.slice(-8)}`,
+        "neo_MerchantTxnID": `${`${Date.now()}`.slice(-8)}`, // unique per request
+        "neo_OrderID": "DH21895330",
         "neo_OrderInfo": "Thanh toán ĐH Test",
         "neo_Title": "Thanh toán",
-        "neo_ReturnURL": "https://tinhte.vn/",
-        "neo_AgainURL": "https://uat-api.neopay.vn/pg/paygate/tryitnow/",
+        "neo_ReturnURL": "https://your-website-domain/return",
     }
-    const hashKey = "123456"
+    const hashKey = "4F99C21FE8A14FD198FA00D"
     const url = generatorUrlRequest(obj, hashKey);
     console.log(url)
 }
